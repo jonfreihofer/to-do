6f568f49428d4827a7d34c6a793439d3a4bba36a
Jonathan Freihofer <patientfrei@gmail.com>
2021-08-30 20:41:44 -0400

    colors set up

---
+++ src/components/Button/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import colors from "../../design/colors";
+export const StyledButton = styled.button`
+  background: ${colors.mainColor};
+  color: white;
+`;
