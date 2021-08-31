1f3cb07f9481a426dfd113f225d5d1229b5a1423
Jonathan Freihofer <patientfrei@gmail.com>
2021-08-30 21:18:14 -0400

    finishing form and lock icon styles

--- src/pages/LogIn/index.js
+++ src/pages/LogIn/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import "../../App.css";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
+import { FaLock } from "react-icons/fa";
 
 export default function LogIn({ children }) {
   return (
     <div className="container-main">
       <header className="title">Rapptr Labs</header>
       <Input />
-      <br />
       <Input />
+      <FaLock className="form-icon lock" />
       <Button>Log In</Button>
     </div>
   );
