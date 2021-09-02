9ee7171349498d258b833e7828f3688c5646247a
Jonathan Freihofer <patientfrei@gmail.com>
2021-09-01 22:15:15 -0400

    todo works now

--- src/components/ToDo/index.js
+++ src/components/ToDo/index.js
@@ -1,6 +1,66 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../../context";
+import "../../App.css";
 import { StyledToDo } from "./styles";
+import { StyledInput } from "../../components/Input/styles.js";
+import { StyledButton } from "../../components/Button/styles.js";
+import { FaPencilAlt, FaTrash } from "react-icons/fa";
 
 export default function ToDo({ children }) {
-  return <StyledToDo></StyledToDo>;
+  const [editMode, setEditMode] = useState(true);
+  const [todo, setTodo] = useState({
+    id: 1,
+    item: "",
+  });
+  const handleToDo = (e) => {
+    const { name, value } = e.target;
+    setTodo((prevToDo) => ({ ...prevToDo, [name]: value }));
+    console.log(todo.item);
+  };
+
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+  };
+
+  const saveToDo = () => {
+    setEditMode(!editMode);
+  };
+  const renderButtonOrIcons = () => {
+    if (editMode) {
+      return (
+        <StyledButton regular className="todo-save-btn" onClick={saveToDo}>
+          Save
+        </StyledButton>
+      );
+    } else {
+      return (
+        <>
+          <FaPencilAlt className="todo-icon pencil" onClick={toggleEditMode} />
+          <br />
+          <FaTrash className="todo-icon trash" />
+        </>
+      );
+    }
+  };
+  return (
+    <StyledToDo>
+      <p
+        className="todo-title"
+        style={{ fontSize: "30px", textShadow: "none" }}
+      >
+        {todo.item}
+      </p>
+      {editMode && (
+        <StyledInput
+          regular
+          className="todo-input"
+          type="text"
+          name="item"
+          value={todo.item}
+          onChange={handleToDo}
+        />
+      )}
+      {renderButtonOrIcons()}
+    </StyledToDo>
+  );
 }
--- src/context/index.js
+++ src/context/index.js
@@ -2,6 +2,19 @@ import React, { useState } from "react";
 
 const Context = React.createContext();
 
-function ContextProvider({ children }) {}
+function ContextProvider({ children }) {
+  const [inputToDo, setInputToDo] = useState({});
+
+  const handleToDo = (e) => {
+    const { name, value } = e.target;
+    setInputToDo((prevInputToDo) => ({ ...prevInputToDo, [name]: value }));
+    console.log(inputToDo.todoInput);
+  };
+  return (
+    <Context.Provider value={(handleToDo, inputToDo, setInputToDo)}>
+      {children}
+    </Context.Provider>
+  );
+}
 
 export { Context, ContextProvider };
